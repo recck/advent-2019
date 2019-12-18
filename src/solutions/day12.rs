deb076e1f3a88c1d42c69ee52a039e36425a6cab
Marcus Recck <mrecck@datto.com>
2019-12-18 14:40:35 -0500

    setup day12

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,3 +1,6 @@
+use itertools::iproduct;
+use regex::Regex;
+
 struct Position {
     x: i32,
     y: i32,
@@ -11,13 +14,52 @@ struct Velocity {
 }
 
 struct Moon {
-    id: i32,
+    id: usize,
     position: Position,
     velocity: Velocity,
 }
 
 pub fn solve(input: String) {
-    let mut input_string: Vec<String> = input.trim_end().split_whitespace().collect();
+    let re = Regex::new(r"[<>]").unwrap();
+    let fixed_input = re.replace_all(&input, "");
+    let input_string: Vec<Vec<i32>> = fixed_input
+        .trim_end()
+        .split_terminator("\n")
+        .map(|s| {
+            s.split(',')
+                .map(|s| s.trim().split('=').nth(1).unwrap().parse().unwrap())
+                .collect()
+        })
+        .collect();
 
-    println!("{:?}", input_string);
+    let mut moons: Vec<Moon> = Vec::new();
+
+    for (i, moon) in input_string.iter().enumerate() {
+        let velocity: Velocity = Velocity { x: 0, y: 0, z: 0 };
+        let position: Position = Position {
+            x: moon[0],
+            y: moon[1],
+            z: moon[2],
+        };
+        moons.push(Moon {
+            id: i,
+            velocity: velocity,
+            position: position,
+        });
+    }
+
+    for (&mut moon_a, &mut moon_b) in iproduct!(moons.iter(), moons.iter()) {
+        if moon_a.id == moon_b.id {
+            continue;
+        }
+
+        compare_moons(*moon_a, *moon_b);
+    }
+}
+
+fn compare_moons(moon_a: &mut Moon, moon_b: &mut Moon) -> () {
+    if moon_a.position.x < moon_b.position.x {
+        moon_a.position.x += 1;
+        moon_b.position.x -= 1;
+    }
 }
